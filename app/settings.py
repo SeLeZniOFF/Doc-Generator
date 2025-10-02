e08acc1da3c999155980ff24e4c256d8a24670df
Селезнев Илья Олегович <46225413+SeLeZniOFF@users.noreply.github.com>
2025-10-03 02:19:23 +0300

    Create settings.py

---
+++ app/settings.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+class Settings(BaseModel):
+DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql+psycopg://postgres:postgres@localhost:5432/docgen")
+STORAGE_DIR: str = os.getenv("STORAGE_DIR", "./storage")
+ALLOWED_ORIGINS: str = os.getenv("ALLOWED_ORIGINS", "http://localhost:8000")
+APP_HOST: str = os.getenv("APP_HOST", "0.0.0.0")
+APP_PORT: int = int(os.getenv("APP_PORT", "8000"))
+
+
+settings = Settings()
