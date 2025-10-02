400b1a0b1d463ad107d012c55bf6c482d7ae953a
Селезнев Илья Олегович <46225413+SeLeZniOFF@users.noreply.github.com>
2025-10-03 02:24:25 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,22 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from .settings import settings
 
-
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
 class Base(DeclarativeBase):
-pass
-
+    pass
 
 # dependency
 from typing import Generator
 
-
 def get_db() -> Generator:
-db = SessionLocal()
-try:
-yield db
-finally:
-db.close()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
